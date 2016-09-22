d0eb0aa103f3b23c6c7fb8984672d7dea3067bd7
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-09-22 16:06:32 +0300

    Cleaned up the code

--- erik.py
+++ erik.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-red = 10  # GPIO number where the led is connected
-green = 22
-blue = 27
+# For I600 RaspberyPi v2
+red = 10  # GPIO number where red led is connected
+green = 22 # GPIO number where green led is connected
+blue = 27 # GPIO number where blue led is connected
 
 GPIO.setmode(GPIO.BCM)
 
@@ -11,34 +12,20 @@ GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
-# Blink some leds
-# while True:
-#     GPIO.output(red, False)
-#     sleep(1)  # Sleep for 1 second
-#     GPIO.output(red, False)
-#     GPIO.output(blue, True)
-#     sleep(1)
-#     GPIO.output(blue, False)
-#     GPIO.output(green, True)
-#     sleep(1)
-#     GPIO.output(green, False)
+# Reset led light
 def cleanLed():
   GPIO.output(red, True)
   GPIO.output(blue, True)
   GPIO.output(green, True)
 
+# turn on red led
 def redOn():
   GPIO.output(red, False)
 
+# turn on blue led
 def blueOn():
   GPIO.output(blue, False)
 
+# turn on green led
 def greenOn():
   GPIO.output(green, False)
-
-# def main():
-#   cleanLed()
-#   blueOn()
-
-# if __name__ == "__main__":
-#     main()
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 # make_server is used to create this simple python webserver
 from wsgiref.simple_server import make_server
+# import led control library
 import erik
 
 # Function that is ran when a http request comes in
@@ -10,24 +11,48 @@ def light_app(env, start_response):
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
 
-    # What did the user ask for?
+    # Checks what path is sent via GET
     if env["PATH_INFO"] == "/on":
         print("user asked for /on")
         erik.cleanLed()
         erik.redOn()
-        return "<!DOCTYPE html><html><head><title>\"Lights\"</title></head><body><a title=\"off\" href=\"/off\">Off</a></body></html>"
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="off" href="/off">Off</a>'\
+                    '</body>'\
+                '</html>'
 
     elif env["PATH_INFO"] == "/off":
         print("user asked for /off")
         erik.cleanLed()
-        return "<!DOCTYPE html><html><head><title>\"Lights\"</title></head><body><a title=\"On\" href=\"/on\">On</a></body></html>"
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="On" href="/on">On</a>'\
+                    '</body>'\
+                '</html>'
     else:
         print("user asked for something else")
-        return "<!DOCTYPE html><html><head><title>\"Lights\"</title></head><body><a title=\"On\" href=\"/on\">On</a></body></html>"
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="On" href="/on">On</a>'\
+                    '</body>'\
+                '</html>'
 
 # Create a small python server
-httpd = make_server("", 8000, light_app)
-print "Serving on port 8000..."
-print "You can open this in the browser http://192.168.1.xxx:8000 where xxx is your rpi ip aadress"
-print "Or if you run this server on your own computer then http://localhost:8000"
+httpd = make_server("", 8437, light_app)
+print "Serving on port 8437..."
+print "You can open this in the browser http://192.168.1.xxx:8437 where xxx is your rpi ip aadress"
+print "Or if you run this server on your own computer then http://localhost:8437"
 httpd.serve_forever()
